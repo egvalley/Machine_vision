74949ebf6671e812c1a1e4c0e851399fb6196698
egvalley <125441543+egvalley@users.noreply.github.com>
2023-03-13 14:50:58 +0800

    Add files via upload

---
+++ PCA_and_SVD/PCA.m
@@ -0,0 +1,31 @@
+clear all
+clc
+
+% Load data
+load('traindata.mat');
+data=traindata;
+[m, n] = size(data);
+
+% Center the data
+mean_data = mean(data);
+data_centered = data - repmat(mean_data, m, 1);
+
+% Compute the covariance matrix
+covariance_matrix = (data_centered' * data_centered) / (m - 1);
+
+% Compute the eigenvectors and eigenvalues of the covariance matrix
+[eigenvectors, eigenvalues] = eig(covariance_matrix);
+
+% Sort the eigenvalues in descending order
+[eigenvalues_sorted, index] = sort(diag(eigenvalues), 'descend');
+eigenvectors_sorted = eigenvectors(:, index);
+
+% Compute the principal components
+score = data_centered * eigenvectors_sorted;
+
+% Select the first(or any number you want) principal components
+score_1d = score(:, 1);
+
+
+
+
